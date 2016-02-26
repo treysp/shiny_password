c51f2e67940d136c976876ca8affcf6b22b38ab7
treysp <treyspiller@gmail.com>
2016-02-26 17:17:31 -0500

    Move entire UI to server file

--- ui.R
+++ ui.R
@@ -1,16 +1,3 @@
-shinyUI(fluidPage(
-  # Centered login interface
-  fluidRow(
-    column(width = 2, offset = 5,
-      br(), br(), br(), br(),
-      uiOutput("uiLogin"),
-      uiOutput("pass")
-    ), 
-  
-  # Slider input
-  uiOutput("obs"),
-
-  # Histogram output
-  plotOutput("distPlot")
-  )
-))
+shinyUI(
+  uiOutput("ui_general")
+)
