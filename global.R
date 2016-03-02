1b7e81226984cd8a79286eb377c39c829ad9cdff
treysp <treyspiller@gmail.com>
2016-03-02 06:53:58 -0500

    Initial commit. Move credentials data import to global.R file

---
+++ global.R
@@ -0,0 +1,9 @@
+library(shiny)
+library(digest)
+
+# credentials data frame for testing (username = "test" and password = "password")
+# NOTE: in real use this would open an existing data frame containing user credentials
+credentials <- data.frame(user = "test", pw = "380796939c86c55d6aa8ea8c941f7652", 
+                          stringsAsFactors = FALSE)
+
+
