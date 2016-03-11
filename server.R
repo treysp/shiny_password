6b3ca44c9d8cf74caa68cb90488feb7d23678be9
treysp <treyspiller@gmail.com>
2016-03-11 16:04:25 -0500

    Add functionality to read, determine, and write user lockout status

--- server.R
+++ server.R
@@ -58,23 +58,46 @@ shinyServer(function(input, output, session) {
   #       is bad (username precedent over pw) or he is locked out. set status value for
   #       error message code below
   observeEvent(input$login_button, {
-    row.username <- which(credentials$user == input$user_name)
-    row.password <- which(credentials$pw == digest(input$password)) # digest() makes md5 hash of password
+    # credentials <- readRDS("credentials/credentials.rds")
     
-    if (length(row.username) == 1 && 
-        length(row.password) >= 1 &&
-        (row.username %in% row.password)) { # more than one user may have same pw
+    row_username <- which(credentials$user == input$user_name)
+    row_password <- which(credentials$pw == digest(input$password)) # digest() makes md5 hash of password
+
+    # if user name row and password name row are same, credentials are valid
+    #   and retrieve locked out status
+    if (length(row_username) == 1 && 
+        length(row_password) >= 1 &&  # more than one user may have same pw
+        (row_username %in% row_password)) {
+      user_input$valid_credentials <- TRUE
+      user_input$user_locked_out <- credentials$locked_out[row_username]
+    }
+
+    # if user is not currently locked out but has now failed login too many times,
+    #   set locked out status in credentials DF to TRUE and save DF
+    if (input$login_button > num_fails_to_lockout & 
+        user_input$user_locked_out == FALSE) {
+      credentials$locked_out[row_username] <- TRUE
+      # saveRDS(credentials, "credentials/credentials.rds")
+      
+      user_input$user_locked_out <- TRUE
+    }
+      
+    # if a user has valid credentials and is not locked out, he is authenticated      
+    if (user_input$valid_credentials == TRUE & user_input$user_locked_out == FALSE) {
       user_input$authenticated <- TRUE
     } else {
       user_input$authenticated <- FALSE
     }
-    
+
+    # if user is not authenticated, set login status variable for error messages below
     if (user_input$authenticated == FALSE) {
-      if (length(row.username) > 1) {
+      if (user_input$user_locked_out == TRUE) {
+        user_input$status <- "locked_out"  
+      } else if (length(row_username) > 1) {
         user_input$status <- "credentials_data_error"  
-      } else if (input$user_name == "" || length(row.username) == 0) {
+      } else if (input$user_name == "" || length(row_username) == 0) {
         user_input$status <- "bad_user"
-      } else if (input$password == "" || length(row.password) == 0) {
+      } else if (input$password == "" || length(row_password) == 0) {
         user_input$status <- "bad_password"
       }
     }
