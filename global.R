588275548a585ff8a6e475bc7bc7234d12314b9a
treysp <treyspiller@gmail.com>
2016-03-11 15:03:43 -0500

    Add lockout status variable to credentials data frame and global variable setting number of failed attempts allowed before lockout.

--- global.R
+++ global.R
@@ -3,7 +3,9 @@ library(digest)
 
 # credentials data frame for testing (username = "test" and password = "password")
 # NOTE: in real use this would open an existing data frame containing user credentials
-credentials <- data.frame(user = "test", pw = "380796939c86c55d6aa8ea8c941f7652", 
+credentials <- data.frame(user = "test", 
+                          pw = "380796939c86c55d6aa8ea8c941f7652", 
+                          locked_out = FALSE,
                           stringsAsFactors = FALSE)
 
-
+num_fails_to_lockout <- 2
