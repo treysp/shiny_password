f5f0a34b60dd6c219f44565b066bab849b6bc185
treysp <treyspiller@gmail.com>
2016-03-28 15:14:41 -0400

    Add warning about testing credentials code. Only create credentials DF from scratch if one is not present in the credentials folder.

--- global.R
+++ global.R
@@ -1,11 +1,22 @@
 library(shiny)
 library(digest)
 
+############# FOR TESTING ONLY - DELETE THIS CODE before using app!!!
+
 # credentials data frame for testing (username = "test" and password = "password")
 # NOTE: in real use this would open an existing data frame containing user credentials
-credentials <- data.frame(user = "test", 
-                          pw = "380796939c86c55d6aa8ea8c941f7652", 
-                          locked_out = FALSE,
-                          stringsAsFactors = FALSE)
 
-num_fails_to_lockout <- 2
+  if (!file.exists("credentials/credentials.rds")) {
+    credentials <- data.frame(user = "test", 
+                              pw = "380796939c86c55d6aa8ea8c941f7652", 
+                              locked_out = FALSE,
+                              stringsAsFactors = FALSE)
+    
+    saveRDS(credentials, "credentials/credentials.rds")
+  }
+    
+############# CODE ABOVE FOR TESTING ONLY!!!
+  
+# set the number of failed attempts allowed before user is locked out
+
+num_fails_to_lockout <- 3
