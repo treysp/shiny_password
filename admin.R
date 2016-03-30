e71697a07b9d5b11da2daf2e5633c30f1439b8c1
treysp <treyspiller@gmail.com>
2016-03-30 10:58:35 -0400

    Delete add_user() function; make delete_users() function able to delete multiple users in one call; fix bugs; add more error checking.

--- admin.R
+++ admin.R
@@ -28,93 +28,51 @@ credentials_init <- function() {
   invisible(TRUE)
 }  
   
-#' Add a single user to the credentials data frame
+#' Add users to the credentials data frame
 #'
-#' Reads the credentials data frame, adds a single user, and saves the credentials data frame.
+#' Reads the credentials data frame, adds users, and saves the credentials data frame. 
+#' When adding multiple users, the two input character vectors should contain pairs of 
+#' user names and passwords in the same locations within the vectors. For example, the 
+#' first user name in the user names vector will be paired with the first password in 
+#' the passwords vector.
 #' 
-#' @param user A character string
-#' @param pw A character string 
+#' @param users A character singleton or vector with no values of NA or ""
+#' @param pws A character singleton or vector with no values of NA or ""
 #'
 #' @return This function is exclusively used for its side effects and returns TRUE invisibly 
 #'  if it executes successfully.
 #'
 #' @examples
-#' add_one_user("user1", "password1")
-add_one_user <- function(user, pw) {
-  require(digest)
-  
-  # check inputs
-  if (!is.character(user) || !(length(user) == 1) || is.na(user) || user == "") {
-    stop("User name must be a non-blank character string of length 1 (cannot be a vector).")
-  }
-  if (!is.character(pw) || !(length(pw) == 1) || is.na(pw) || pw == "") {
-    stop("User password must be a non-blank character string of length 1 (cannot be a vector).")
-  }
-
-  # add user
-  credentials <- readRDS("credentials/credentials.RDS")
-  
-  if (user %in% credentials[, "user"]) stop("User already exists - choose a different user name.")
-  
-  row <- nrow(credentials) + 1
-  
-  credentials[row, "user"] <- user
-  credentials[row, "pw"] <- digest(pw)
-  credentials[row, "locked_out"] <- FALSE
-  
-  # check for rows with blank user names or password
-  if (any(is.na(credentials[, "user"]) | 
-      credentials[, "user"] == "")) {
-    stop("An entry in the credentials data frame is missing a user name - please correct.")
-  }
-  if (any(is.na(credentials[, "pw"]) | 
-      credentials[, "pw"] == "")) {
-    stop("An entry in the credentials data frame is missing a password - please correct.")    
-  }
-  
-  saveRDS(credentials, "credentials/credentials.rds")
-  invisible(TRUE)
-}
-
-#' Add multiple users to the credentials data frame
-#'
-#' Reads the credentials data frame, adds multiple users, and saves the credentials data frame. 
-#' The two input character vectors should contain pairs of user names and passwords in the same
-#' locations within the vectors. For example, the first user name in the user names vector will
-#' be paired with the first password in the passwords vector.
+#' add_users("user1", "password1")
 #' 
-#' @param users A character vector with no values of NA or ""
-#' @param pws A character vector with no values of NA or ""
-#'
-#' @return This function is exclusively used for its side effects and returns TRUE invisibly 
-#'  if it executes successfully.
-#'
-#' @examples
-#' add_multiple_users(c("user1", "user2"), c("password1", "password2"))
-add_multiple_users <- function(users, pws) {
-  require(digest)
+#' users_to_add <- c("user2", "user3")
+#' passwords_to_add <- c("password2", "password3")
+#' add_users(user_to_add, passwords_to_add)
+add_users <- function(users, pws) {
+  library(digest)
   
   # check inputs
-  if (!is.character(users) || !is.vector(users) || any(is.na(users)) || any(users == "")) {
-    stop("User names must be a character vector with no blank entries.")
+  if (!is.character(users) || any(is.na(users)) || any(users == "")) {
+    stop("User names must be characters. User names cannot be NA or \"\".")
   }
-  if (!is.character(pws) || !is.vector(pws) || any(is.na(pws)) || any(pws == "")) {
-    stop("User passwords must be a character vector with no blank entries.")
+  if (length(unique(users)) != length(users)) stop("You cannot add multiple users with the same user name.")
+  if (!is.character(pws) || any(is.na(pws)) || any(pws == "")) {
+    stop("Passwords must be characters. Passwords cannot be NA or \"\".")
   }
-  if (length(users) != length(pws)) stop("Users and passwords vectors are not the same length.")
+  if (length(users) != length(pws)) stop("You must have the same number of passwords as users.")
   
   # add users
   credentials <- readRDS("credentials/credentials.RDS")
   
-  if (any(users %in% credentials[, "user"])) {
-    dupe_users <- credentials[users %in% credentials[, "user"], "user"]
+  if (any(credentials[, "user"] %in% users)) {
+    dupe_users <- credentials[which(credentials[, "user"] %in% users), "user"]
     
     dupe_users <- paste(dupe_users, collapse = ", ")
     message <- paste0("Users [", dupe_users, "] already exist - choose different user names.")
     stop(message)
   }
   
-  temp_df <- data.frame(user = users, pw = digest(pws), 
+  temp_df <- data.frame(user = users, pw = sapply(pws, FUN = digest), 
                         locked_out = rep(FALSE, times = length(users)), 
                         stringsAsFactors = FALSE)
   
@@ -133,31 +91,43 @@ add_multiple_users <- function(users, pws) {
   invisible(TRUE)
 }
 
-#' Delete a single user from the credentials data frame
+#' Delete users from the credentials data frame
 #'
-#' Reads the credentials data frame, deletes a single user, and saves the credentials data frame.
+#' Reads the credentials data frame, deletes users, and saves the credentials data frame.
 #' 
-#' @param user A character string
+#' @param users A character singleton or vector with no values of NA or ""
 #'
 #' @return This function is exclusively used for its side effects and returns TRUE invisibly 
 #'  if it executes successfully.
 #'
 #' @examples
-#' delete_user("user1")
-delete_user <- function(user) {
+#' delete_users("user1")
+#' 
+#' users_to_delete <- c("user1", "user2")
+#' delete_users(user_to_delete)
+delete_users <- function(users) {
   # check input
-  if (!is.character(user) || !(length(user) == 1) || is.na(user) || user == "") {
-    stop("User name must be a non-blank character string of length 1 (cannot be a vector).")
+  if (!is.character(users) || any(is.na(users)) || any(users == "")) {
+    stop("User names must be characters. User names cannot be NA or \"\".")
+  }
+  if (length(unique(users)) != length(users)) {
+    warning("Your list of users to delete has a duplicated user name in it.")
   }
 
-  # delete user
+  # delete users
   credentials <- readRDS("credentials/credentials.RDS")
   
-  if (!(user %in% credentials[, "user"])) stop("User name does not exist in credentials data.")
+  if (any(!(users %in% credentials[, "user"]))) {
+    bad_users <- users[!(users %in% credentials[, "user"])]
+    
+    bad_users <- paste(bad_users, collapse = ", ")
+    message <- paste0("Users [", bad_users, "] are not in the credentials data - please correct.")
+    stop(message)
+  }
+
+  row_username <- which(credentials[, "user"] %in% users)
   
-  row_username <- which(credentials$user == user)
-  
-  if (length(row_username) > 1) stop("Credentials data error - more than one user has this user name.")
+  if (length(row_username) > length(users)) stop("Credentials data error - more than one user has the same user name.")
   
   credentials <- credentials[-row_username, ]
 
